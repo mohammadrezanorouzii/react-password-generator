0c3ea38cff08c5612989acfa2cacc9b27309021d
MohammadReza <mohammadblack75@gmail.com>
2021-10-25 11:58:10 +0330

    add copy button

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import "./App.css";
 import { useEffect, useState } from "react";
+import cn from "classnames";
+import Copy from "./copy";
 
 function App() {
 
@@ -12,18 +14,9 @@ function App() {
   const [trueCount, trueSetter] = useState(0);
   const [password, passSetter] = useState(" ")
 
-  // we should do smoething that all buttons cant be false together
-
   useEffect(() => {
 
     //================== bulding the password ==================
-
-    console.log("count : " + count);
-
-    console.log(nums);
-    console.log(chars);
-    console.log(upper);
-    console.log(lower);
 
     let ALLNUMBERS = [0,1,2,3,4,5,6,7,8,9];
     let passwordArray = [];
@@ -93,23 +86,26 @@ function App() {
     
     console.log("true count = " + trueCount);
 
-    if (count > 12 && trueCount > 0 ){
+    if (count > 11 && trueCount > 0 ){
       powerSetter(100);
     }
     
-    if(count < 12 && count > 8 ){
+    if(count < 11 && count > 8 ){
       if (trueCount === 1) {
-            powerSetter(60);
+        powerSetter(60);
       }
-      if (trueCount === 0){
+      else if (trueCount === 0){
         powerSetter(0);
+      }
+      else if (trueCount === 2){
+        powerSetter(60)
       }
       else {
         powerSetter(100);
       }
     }
 
-    if (count<9 && count>5){
+    if (count < 9 && count > 5){
       if (trueCount === 1){
         powerSetter(28)    
       }
@@ -130,70 +126,92 @@ function App() {
       powerSetter(28);
     }
 
-    if (passwordPower === 28){
-      
+    if (trueCount === 0 || count === 0){
+      powerSetter(0)
     }
 
   },[upper,lower,nums,chars,count,trueCount])
 
   const changeHandler = (e) => {
     counter(e.target.value);
-
   };
+
+  function copy() {
+    navigator.clipboard.writeText(password);
+  }
 
   return (
 
     <div className="App">
       <div className="container">
 
-        <h1 className="myh2"> { password} </h1>
-
-        <div className="switch1-2">
-          <div className="switch1">
-            <input
-              type="checkbox"
-              id="checkbox1"
-              onChange={() => setNums(t => !t)}
-              defaultChecked={nums}
-              value={nums}
-            />
-            <p className="p-1"> Numbers </p>
-          </div>
-          <div className="switch2">
-            <input
-              type="checkbox"
-              id="checkbox2"
-              onChange={() => setChars(t => !t)}
-              defaultChecked={chars}
-              value={chars}
-            />
-            <p className="p-1"> !@#$% </p>
+        <div className="pass-container">
+          <h1 className="myh1"> {password} </h1>
+          <div className="copy-container">
+            <button className="btn-copy" onClick={copy}> <Copy/> </button>
           </div>
         </div>
 
-        <div className="switch3-4">
-          <div className="switch3">
-            <input
-              type="checkbox"
-              id="checkbox3"
-              onChange={() => setUpper(t => !t)}
-              defaultChecked={upper}
-              value={upper}
-            />
-            <p className="p-1"> UpperCase </p>
+          <div className="progress">
+            <div className={cn({
+              ["progress-bar"] : true ,
+              ["progress-bar0"] : passwordPower === 0,
+              ["progress-bar28"] : passwordPower === 28,
+              ["progress-bar60"] : passwordPower === 60,
+              ["progress-bar100"] : passwordPower === 100
+              })} >
+            </div>
+          </div>
+        
+        <div className="all-switches">
+          <div className="switch1-2">
+            <div className="switch1">
+              <input
+                type="checkbox"
+                id="checkbox1"
+                onChange={() => setNums(t => !t)}
+                defaultChecked={nums}
+                value={nums}
+              />
+              <p className="p-1"><b> Numbers </b></p>
+            </div>
+            <div className="switch2">
+              <input
+                type="checkbox"
+                id="checkbox2"
+                onChange={() => setChars(t => !t)}
+                defaultChecked={chars}
+                value={chars}
+              />
+              <p className="p-1"><b> !@#$% </b></p>
+            </div>
           </div>
 
-          <div className="switch4">
-            <input
-              type="checkbox"
-              id="checkbox4"
-              onChange={() => setLower(t => !t)}
-              defaultChecked={lower}
-              value={lower}
-            />
-            <p className="p-1"> LowerCase </p>
-          </div> 
+          <div className="switch3-4">
+            <div className="switch3">
+              <input
+                type="checkbox"
+                id="checkbox3"
+                onChange={() => setUpper(t => !t)}
+                defaultChecked={upper}
+                value={upper}
+              />
+              <p className="p-1"><b> UpperCase </b></p>
+            </div>
+
+            <div className="switch4">
+              <input
+                type="checkbox"
+                id="checkbox4"
+                onChange={() => setLower(t => !t)}
+                defaultChecked={lower}
+                value={lower}
+              />
+              <p className="p-1"><b> LowerCase </b></p>
+            </div> 
+          </div>
         </div>
+
 
         <div className="input-container">
           <div className="start-end">
@@ -212,19 +230,7 @@ function App() {
           <span className="count"> Password Length : {count} </span>
 
         </div>
-
-        <div className="progress-container">
-          <div className="progress-div">
-            <p className="quality-1">weak</p>
-            <p className="quality-2">normal</p>
-            <p className="quality-3">strong</p>
-          </div>
-
-          <div className="progress">
-            <div className="progress-bar"></div>
-          </div>
-        </div>
-
+        
       </div>
     </div>
   );
